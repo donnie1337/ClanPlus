869784529fd7d7588cd8784bae1ee7d0ce4af29f
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-12 01:35:46 -0300

    feat: move player profile head to slot 17

--- src/main/java/com/donnie1337/clanplus/ClanGui.java
+++ src/main/java/com/donnie1337/clanplus/ClanGui.java
@@ -53,12 +53,11 @@ public final class ClanGui {
                 color("&7Coins: &6" + coins(p))
         ));
         profile.setItemMeta(skull);
-        inv.setItem(4, profile);
+        inv.setItem(17, profile);
         inv.setItem(11, item(c == null ? Material.EMERALD : Material.CHEST, c == null ? "&aCriar Clan" : "&bMeu Clan", c == null ? "&7Crie seu próprio clan." : "&7Gerencie seu clan."));
         inv.setItem(13, item(Material.WRITABLE_BOOK, "&eConvites recebidos", "&7Veja e aceite seus convites."));
         inv.setItem(14, item(Material.NETHER_STAR, "&6Clans mais Top", "&7Ranking baseado no KDR médio", "&7dos jogadores de cada clan."));
         inv.setItem(16, item(Material.NAME_TAG, "&fClans do servidor", "&7Veja todos os clans existentes."));
-        inv.setItem(17, item(Material.DIAMOND_SWORD, "&cRanking de KDR", "&7Veja os melhores jogadores."));
         p.openInventory(inv);
     }
     public void openInvites(Player p) {
